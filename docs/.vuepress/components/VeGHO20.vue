17e6654710be293eeccb2b98c157157d896a7df6
Luigi <luigibyte@gmail.com>
2024-01-19 18:51:59 -0300

    pool selector

--- docs/.vuepress/components/VeGHO20.vue
+++ docs/.vuepress/components/VeGHO20.vue
@@ -3,19 +3,25 @@ import { createProviderComponent } from '../providers/createProviderComponent';
 import { provideNetwork } from '../providers/network';
 import { provideVeSystem } from '../providers/veSystem';
 import { provideTabs, Tab } from '../providers/tabs';
+import { providePools } from '../providers/pools';
 import TabsComponent from './vegho20/admin/TabsComponent.vue';
 
 const NetworkProvider = createProviderComponent(() => provideNetwork());
 const VeSystemProvider = createProviderComponent(() => provideVeSystem());
 const TabsProvider = createProviderComponent(() => provideTabs(Tab.LAUNCHPAD));
+const GlobalProvider = createProviderComponent(() => {
+  providePools();
+});
 </script>
 
 <template>
   <NetworkProvider>
-    <VeSystemProvider>
-      <TabsProvider>
-        <TabsComponent />
-      </TabsProvider>
-    </VeSystemProvider>
+    <GlobalProvider>
+      <VeSystemProvider>
+        <TabsProvider>
+          <TabsComponent />
+        </TabsProvider>
+      </VeSystemProvider>
+    </GlobalProvider>
   </NetworkProvider>
 </template>
