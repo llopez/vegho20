1b046d3922df6f417e5db332c2a6b16487c22790
Luigi <luigibyte@gmail.com>
2024-01-20 22:58:12 -0300

    activate rewards tabs

---
+++ docs/.vuepress/components/VeGHO20ActivateRewards.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { createProviderComponent } from '../providers/createProviderComponent';
+import { provideNetwork } from '../providers/network';
+import { provideVeSystem } from '../providers/veSystem';
+import { provideTabs, Tab } from '../providers/tabs';
+import { providePools } from '../providers/pools';
+import TabsComponent from './vegho20/ActivateRewards/TabsComponent.vue';
+
+const NetworkProvider = createProviderComponent(() => provideNetwork());
+const VeSystemProvider = createProviderComponent(() => provideVeSystem());
+const TabsProvider = createProviderComponent(() => provideTabs(Tab.FORM_1));
+const GlobalProvider = createProviderComponent(() => {
+  providePools();
+});
+</script>
+
+<template>
+  <NetworkProvider>
+    <GlobalProvider>
+      <VeSystemProvider>
+        <TabsProvider>
+          <TabsComponent />
+        </TabsProvider>
+      </VeSystemProvider>
+    </GlobalProvider>
+  </NetworkProvider>
+</template>
